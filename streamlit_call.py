25b2ae33f472d06789b8e56a0c72ec4e140318f4
Kemal <87644980+kemlvm@users.noreply.github.com>
2023-02-07 20:46:36 +0300

    Update streamlit_call.py

--- streamlit_call.py
+++ streamlit_call.py
@@ -7,7 +7,6 @@ def load_jupyter_server_extension(nbapp):
             "streamlit",
             "run",
             "streamlit_app.py",
-            "--browser.serverAddress=0.0.0.0",
             "--server.enableCORS=False",
         ]
     )
